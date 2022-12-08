846269dd977c6a4707871584da5cafdca03c4ee1
bor.osbalazs <62463956+bor-osbalazs@users.noreply.github.com>
2022-12-08 03:20:44 +0100

    Updated gitignore and created first files

---
+++ CSharp/AdventOfCode/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Core
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
