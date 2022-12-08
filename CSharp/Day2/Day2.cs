5533f1b0686f98d495114bce685b436fded90081
bor.osbalazs <62463956+bor-osbalazs@users.noreply.github.com>
2022-12-08 23:19:34 +0100

    Reworked the library

---
+++ CSharp/Day2/Day2.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class RockPaperScissorsSimulator
+    {
+        private string inputPath;
+        private int points;
+        private int pointsTwo;
+        private Dictionary<string, int> pointDictionary = new Dictionary<string, int>();
+
+        public RockPaperScissorsSimulator()
+        {
+            this.inputPath = $"{Core.directoryPath}/Day2-Input.txt";
+
+            PopulateDictionary();
+            CalculatePoint();
+        }
+
+        private void PopulateDictionary()
+        {
+            /*
+            Opponent        Response
+            A: Rock         X: Rock
+            B: Paper        Y: Paper
+            C: Scissors     Z: Scissors
+            */
+
+            pointDictionary.Add("X", 1);
+            pointDictionary.Add("Y", 2);
+            pointDictionary.Add("Z", 3);
+            pointDictionary.Add("Lose", 0);
+            pointDictionary.Add("Draw", 3);
+            pointDictionary.Add("Win", 6);
+        }
+
+        private void CalculatePoint()
+        {
+
+            using(StreamReader reader = new StreamReader(inputPath))
+            {
+                while(reader.EndOfStream == false)
+                {
+                    string currentLine = reader.ReadLine();
+
+                    if (pointDictionary.TryGetValue(currentLine.Split()[1], out int symbolPoint) == true && 
+                        pointDictionary.TryGetValue(IsWinOrLoseOrDraw(currentLine.Split()[0], currentLine.Split()[1]), out int statusPoint) == true)
+                    {
+                        points += symbolPoint;
+                        points += statusPoint;
+                    }
+
+                    if (pointDictionary.TryGetValue(DeterminateOwnSymbol(currentLine.Split()[0], currentLine.Split()[1]), out int symbolPointTwo) == true &&
+                        pointDictionary.TryGetValue(DeterminateGameEnding(currentLine.Split()[1]), out int statusPointTwo) == true)
+                    {
+                        pointsTwo += symbolPointTwo;
+                        pointsTwo += statusPointTwo;
+                    }
+                }
+            }
+        }
+
+        private string IsWinOrLoseOrDraw(string opponentSymbol, string ownSymbol)
+        {
+            string winStatus = "Win";
+            string drawStatus = "Draw";
+            string loseStatus = "Lose";
+
+            string matchStatus = "";
+
+            if (opponentSymbol == "A" && ownSymbol == "Y")
+            {
+                matchStatus = winStatus;
+            }
+            else if (opponentSymbol == "B" && ownSymbol == "Z")
+            {
+                matchStatus = winStatus;
+            }
+            else if (opponentSymbol == "C" && ownSymbol == "X")
+            {
+                matchStatus = winStatus;
+            }
+            else if (opponentSymbol == "A" && ownSymbol == "X")
+            {
+                matchStatus = drawStatus;
+            }
+            else if (opponentSymbol == "B" && ownSymbol == "Y")
+            {
+                matchStatus = drawStatus;
+            }
+            else if (opponentSymbol == "C" && ownSymbol == "Z")
+            {
+                matchStatus = drawStatus;
+            }
+            else if (opponentSymbol == "A" && ownSymbol == "Z")
+            {
+                matchStatus = loseStatus;
+            }
+            else if (opponentSymbol == "B" && ownSymbol == "X")
+            {
+                matchStatus = loseStatus;
+            }
+            else if (opponentSymbol == "C" && ownSymbol == "Y")
+            {
+                matchStatus = loseStatus;
+            }
+
+            return matchStatus;
+        }
+
+        private string DeterminateGameEnding(string ownSymbol)
+        {
+            string winStatus = "Win";
+            string drawStatus = "Draw";
+            string loseStatus = "Lose";
+
+            string matchStatus = "";
+
+            if (ownSymbol == "X")
+            {
+                matchStatus = loseStatus;
+            }
+            else if (ownSymbol == "Y")
+            {
+                matchStatus = drawStatus;
+            }
+            else if (ownSymbol == "Z")
+            {
+                matchStatus = winStatus;
+            }
+
+            return matchStatus;
+        }
+
+        private string DeterminateOwnSymbol(string opponentSymbol, string statusSymbol)
+        {
+            /*
+            Opponent        Response
+            A: Rock         X: Rock
+            B: Paper        Y: Paper
+            C: Scissors     Z: Scissors
+            */
+
+            string rockSymbol = "X";
+            string paperSymbol = "Y";
+            string scissorsSymbol = "Z";
+
+            string ownSymbol = "";
+
+            if (opponentSymbol == "A" && DeterminateGameEnding(statusSymbol) == "Win")
+            {
+                ownSymbol = paperSymbol;
+            }
+            else if (opponentSymbol == "B" && DeterminateGameEnding(statusSymbol) == "Win")
+            {
+                ownSymbol = scissorsSymbol;
+            }
+            else if (opponentSymbol == "C" && DeterminateGameEnding(statusSymbol) == "Win")
+            {
+                ownSymbol = rockSymbol;
+            }
+            else if (opponentSymbol == "A" && DeterminateGameEnding(statusSymbol) == "Draw")
+            {
+                ownSymbol = rockSymbol;
+            }
+            else if (opponentSymbol == "B" && DeterminateGameEnding(statusSymbol) == "Draw")
+            {
+                ownSymbol = paperSymbol;
+            }
+            else if (opponentSymbol == "C" && DeterminateGameEnding(statusSymbol) == "Draw")
+            {
+                ownSymbol = scissorsSymbol;
+            }
+            else if (opponentSymbol == "A" && DeterminateGameEnding(statusSymbol) == "Lose")
+            {
+                ownSymbol = scissorsSymbol;
+            }
+            else if (opponentSymbol == "B" && DeterminateGameEnding(statusSymbol) == "Lose")
+            {
+                ownSymbol = rockSymbol;
+            }
+            else if (opponentSymbol == "C" && DeterminateGameEnding(statusSymbol) == "Lose")
+            {
+                ownSymbol = paperSymbol;
+            }
+
+            return ownSymbol;
+        }
+
+        public void ShowPoints()
+        {
+            Console.WriteLine($"First points: {points}");
+            Console.WriteLine($"Second points: {pointsTwo}");
+        }
+    }
+}
---
+++ CSharp/Day3/Day3.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Reorganizer
+    {
+        string inputPath;
+        Dictionary<string, int> itemPriority = new Dictionary<string, int>();
+        int prioritySum;
+        int badgePrioritySum;
+
+        public Reorganizer()
+        {
+            this.inputPath = $"{Core.directoryPath}/Day3-Input.txt";
+
+            PopulateDictionary();
+            CalculatePrioritySum();
+        }
+
+
+        private void PopulateDictionary()
+        {
+            string alphabetWhole = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int currentLetterIndex = 0; currentLetterIndex < alphabetWhole.Length; currentLetterIndex++)
+            {
+                itemPriority.Add(Convert.ToString(alphabetWhole[currentLetterIndex]), currentLetterIndex + 1);
+            }
+        }
+
+        private void CalculatePrioritySum()
+        {
+            using (StreamReader reader = new StreamReader(inputPath))
+            {
+                int elfCounter = 0;
+                List<string> elvesComparmentList = new List<string>();
+
+                while (reader.EndOfStream == false)
+                {
+                    string currentLine = reader.ReadLine();
+
+                    Tuple<string, string> comparments = new Tuple<string, string>(FindCompartments(currentLine).Item1, FindCompartments(currentLine).Item2);
+                    UpdatePrioritySum(comparments);
+
+                    elvesComparmentList.Add(currentLine);
+
+                    if (elfCounter < 2)
+                    {
+                        elfCounter++;
+                    }
+                    else
+                    {
+                        UpdateBadgePrioritySum(elvesComparmentList);
+                        elvesComparmentList.Clear();
+                        elfCounter = 0;
+                    }
+                }
+            }
+        }
+
+        private void UpdatePrioritySum(Tuple<string, string> comparments)
+        {
+            List<string> alreadyPrioritizedLetter = new List<string>();
+
+            for (int firstComparmentLetterIndex = 0; firstComparmentLetterIndex < comparments.Item1.Length; firstComparmentLetterIndex++)
+            {
+                for (int secondComparmentLetterIndex = 0; secondComparmentLetterIndex < comparments.Item2.Length; secondComparmentLetterIndex++)
+                {
+                    bool isLetterSame = comparments.Item1[firstComparmentLetterIndex] == comparments.Item2[secondComparmentLetterIndex];
+                    bool isLetterInDictionary = itemPriority.TryGetValue(Convert.ToString(comparments.Item1[firstComparmentLetterIndex]), out int priorityValue);
+                    bool isLetterAlreadyPrioritized = alreadyPrioritizedLetter.Contains(Convert.ToString(comparments.Item1[firstComparmentLetterIndex]));
+
+                    if (isLetterSame == true && isLetterInDictionary == true && isLetterAlreadyPrioritized == false)
+                    {
+                        alreadyPrioritizedLetter.Add(Convert.ToString(comparments.Item1[firstComparmentLetterIndex]));
+                        prioritySum += priorityValue;
+                    }
+                }
+            }
+        }
+
+        private void UpdateBadgePrioritySum(List<string> elvesComparmentList)
+        {
+            List<string> alreadyPrioritizedLetter = new List<string>();
+            List<string> commonLettersInFirstTwoLines = FindCommonLetters(elvesComparmentList);
+
+            for (int commonLettersIndex = 0; commonLettersIndex < commonLettersInFirstTwoLines.Count(); commonLettersIndex++)
+            {
+                for (int thirdElfComparmentLetterIndex = 0; thirdElfComparmentLetterIndex < elvesComparmentList[2].Count(); thirdElfComparmentLetterIndex++)
+                {
+                    bool isCommonLetterFoundInThirdLine = commonLettersInFirstTwoLines[commonLettersIndex] == Convert.ToString(elvesComparmentList[2][thirdElfComparmentLetterIndex]);
+                    bool isLetterInDictionary = itemPriority.TryGetValue(commonLettersInFirstTwoLines[commonLettersIndex], out int priorityValue);
+                    bool isAlreadyPrioritized = alreadyPrioritizedLetter.Contains(commonLettersInFirstTwoLines[commonLettersIndex]);
+
+                    if (isCommonLetterFoundInThirdLine == true && isLetterInDictionary == true && isAlreadyPrioritized == false)
+                    {
+                        alreadyPrioritizedLetter.Add(commonLettersInFirstTwoLines[commonLettersIndex]);
+                        badgePrioritySum += priorityValue;
+                    }
+                }
+            }
+        }
+
+        private List<string> FindCommonLetters(List<string> elvesComparmentList)
+        {
+            List<string> commonLetters = new List<string>();
+
+            for (int firstElfComparmentLetterIndex = 0; firstElfComparmentLetterIndex < elvesComparmentList[0].Count(); firstElfComparmentLetterIndex++)
+            {
+                for (int secondElfComparmentLetterIndex = 0; secondElfComparmentLetterIndex < elvesComparmentList[1].Count(); secondElfComparmentLetterIndex++)
+                {
+                    if (elvesComparmentList[0][firstElfComparmentLetterIndex] == elvesComparmentList[1][secondElfComparmentLetterIndex])
+                    {
+                        commonLetters.Add(Convert.ToString(elvesComparmentList[0][firstElfComparmentLetterIndex]));
+                    }
+                }
+            }
+
+            return commonLetters;
+        }
+
+        private (string, string) FindCompartments(string currentLine)
+        {
+            int currentLineHalfLength = currentLine.Length / 2;
+
+            string firstComparment = currentLine.Substring(0, currentLineHalfLength);
+            string secondComparment = currentLine.Substring(currentLineHalfLength, currentLineHalfLength);
+
+            return (firstComparment, secondComparment);
+        }
+
+        public void ShowPrioritySum()
+        {
+            Console.WriteLine($"Priority sum is: {prioritySum} and Badge Priority sum is: {badgePrioritySum}");
+        }
+    }
+}
